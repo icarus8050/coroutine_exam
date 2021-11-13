acde0e49a219a507042f013fa86817d4ebeef992
icarus8050 <icarus8050@naver.com>
2021-11-13 22:53:17 +0900

    컨텍스트 결합

---
+++ src/main/kotlin/ContextCoupling.kt
@@ -0,0 +1,20 @@
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        val dispatcher = newSingleThreadContext("myDispatcher")
+        val handler = CoroutineExceptionHandler { _, throwable ->
+            println("Error captured")
+            println("Message: ${throwable.message}")
+        }
+
+        GlobalScope.launch(dispatcher + handler) {
+            println("Running in ${Thread.currentThread().name}")
+            TODO("Not implemented")
+        }.join()
+    }
+}
