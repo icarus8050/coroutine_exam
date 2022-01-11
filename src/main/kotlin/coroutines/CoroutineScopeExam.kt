1402f8e47fcce76adb9c431eb748cabc0a2be164
icarus8050 <icarus8050@naver.com>
2022-01-12 02:57:16 +0900

    Coroutine Scope Exam

---
+++ src/main/kotlin/coroutines/CoroutineScopeExam.kt
@@ -0,0 +1,13 @@
+package coroutines
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+suspend fun main(): Unit = coroutineScope {
+    launch {
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello, ")
+}
