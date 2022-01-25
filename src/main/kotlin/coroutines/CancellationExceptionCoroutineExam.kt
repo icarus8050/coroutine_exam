23ec28b73fc0efe49cce35489efd46162f997ebe
icarus8050 <icarus8050@naver.com>
2022-01-26 01:57:36 +0900

    코루틴이 cancel 된 이후에는 새로운 코루틴이 생성되더라도 무시된다.

---
+++ src/main/kotlin/coroutines/CancellationExceptionCoroutineExam.kt
@@ -0,0 +1,26 @@
+package coroutines
+
+import kotlinx.coroutines.*
+
+suspend fun main(): Unit = coroutineScope {
+    val job = Job()
+    launch(job) {
+        try {
+            delay(200)
+            println("Job is done")
+        } catch (e: CancellationException) {
+            println(e)
+            throw e
+        } finally {
+            println("Finally")
+            launch {    // will be ignored
+                println("Will not be printed")
+            }
+            delay(100)  // here exception is thrown
+            println("Will not be printed")
+        }
+    }
+    delay(100)
+    job.cancelAndJoin()
+    println("Cancel done")
+}
