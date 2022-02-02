044ff3277667680ca81b734d5001be35a6e5e36d
icarus8050 <icarus8050@naver.com>
2022-02-03 01:02:14 +0900

    try-catch 로 코루틴 컨텍스트에서 발생하는 에러를 캐치하도록 래핑해도 무시되므로 주의해야한다

---
+++ src/main/kotlin/coroutines/exception_handler/BadPracticeTryCatchJobExam.kt
@@ -0,0 +1,22 @@
+package coroutines.exception_handler
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main(): Unit = runBlocking {
+    // Don't wrap in a try-catch here. It will be ignored
+    try {
+        launch {
+            delay(1000)
+            throw Error("Some error")
+        }
+    } catch (e: Throwable) { // nope, does not help here
+        println("Will not be printed")
+    }
+
+    launch {
+        delay(2000)
+        println("Will not be printed")
+    }
+}
