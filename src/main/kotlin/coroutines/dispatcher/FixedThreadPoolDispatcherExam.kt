b7b130a4e23387a57164ef1c58d9b814b3ec5175
icarus8050 <icarus8050@naver.com>
2022-02-13 22:07:42 +0900

    FixedThreadPoolDispatcherExam.kt

---
+++ src/main/kotlin/coroutines/dispatcher/FixedThreadPoolDispatcherExam.kt
@@ -0,0 +1,20 @@
+package coroutines.dispatcher
+
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import java.util.concurrent.Executors
+
+suspend fun main() = coroutineScope {
+    val dispatcher = Executors.newFixedThreadPool(5)
+        .asCoroutineDispatcher()
+    repeat(1000) {
+        launch(dispatcher) {
+            Thread.sleep(200)
+
+            val threadName = Thread.currentThread().name
+            println("Running on thread: $threadName")
+        }
+    }
+    dispatcher.close()
+}
