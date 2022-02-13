567886f4194d2d176e93f64a7d7e19e98f5d93c7
icarus8050 <icarus8050@naver.com>
2022-02-13 22:44:27 +0900

    UnconfinedDispatcherExam

---
+++ src/main/kotlin/coroutines/dispatcher/UnconfinedDispatcherExam.kt
@@ -0,0 +1,29 @@
+package coroutines.dispatcher
+
+import kotlinx.coroutines.*
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+suspend fun main(): Unit = withContext(newSingleThreadContext("Name1")) {
+    var continuation: Continuation<Unit>? = null
+
+    launch(newSingleThreadContext(("Name2"))) {
+        delay(1000)
+        continuation?.resume(Unit)
+    }
+
+    launch(Dispatchers.Unconfined) {
+        println(Thread.currentThread().name)    // Name1
+
+        suspendCoroutine<Unit> { continuation = it }
+
+        println(Thread.currentThread().name)    // Name2
+
+        delay(1000)
+
+        println(Thread.currentThread().name)
+        // kotlinx.coroutines.DefaultExecutor
+        // (used by delay)
+    }
+}
