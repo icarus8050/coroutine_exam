7e6bf135dcba2835b13af519ccfc1a8146169d3b
icarus8050 <icarus8050@naver.com>
2022-04-16 15:50:33 +0900

    SuspendAndContinueExam

---
+++ src/main/kotlin/study/SuspendAndContinueExam.kt
@@ -0,0 +1,15 @@
+package study
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class SuspendAndContinueExam
+
+fun main() = runBlocking {
+    launch {
+        delay(2000L)
+        println("Awake!")
+    }
+    println("코루틴의 일시중단 함수는 쓰레드를 블로킹하지 않는다.")
+}
